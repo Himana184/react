dc9c8f37979c5ae495bf593bc481a3daf922f4fa
Himana184 <109400697+Himana184@users.noreply.github.com>
2022-09-24 09:47:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import ChangeColors from "./ChangeColors";
+
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
+export default App;
